1190f771e0fa8a8aac8086b887f6d00b8c266e14
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2024-10-09 18:11:40 -0300

    Finished code

---
+++ Linked List/README.md
@@ -0,0 +1,2 @@
+# Linked List
+Neste repsitório contem a resolução do exercício 1 da atividade Desafios Lista Encadeada
---
+++ Linked List/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class App {
+    public static Vector<Integer> toVector(LinkedList<Integer> linkedList) {
+        Iterator<Integer> iterator = linkedList.descendingIterator();
+        Vector<Integer> vector = new Vector<>();
+        
+        while (iterator.hasNext()) {
+            vector.add(iterator.next());
+        }
+
+        return vector;
+    }
+
+    public static void main(String[] args) throws Exception {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+        
+        System.out.println(toVector(linkedList));
+    }
+}
